b65e3bf368ab1796337054e1dc07b11951c3a969
Manchakkay Maxim <mxmaster1203@live.com>
2021-12-09 02:12:58 +0800

    Collision tests improved
    Now test for collision between Paddle and Ball give more information about side and type of collision

--- src/collision.lua
+++ src/collision.lua
@@ -42,6 +42,11 @@ function CollisionObject:create( arguments )
 end
 
 function CollisionObject:checkCollision( obj )
+    local result = {
+        success = false,
+        rectSide = "none",
+        type = "none"
+    }
     -- Если это шар
     if (self.type == "ball") then
         -- Соприкасается с прямоугольником
@@ -49,27 +54,60 @@ function CollisionObject:checkCollision( obj )
             local checkX = self.x
             local checkY = self.y
 
+            local sideX = "noneX"
             if (self.x < obj.x) then
                 checkX = obj.x
+                sideX = "left"
             elseif (self.x > obj.x + obj.w) then
                 checkX = obj.x + obj.w
+                sideX = "right"
             end
 
+            local sideY = "noneY"
             if (self.y < obj.y) then
                 checkY = obj.y
+                sideY = "top"
             elseif (self.y > obj.y + obj.h) then
                 checkY = obj.y + obj.h
+                sideY = "bottom"
             end
 
-            local dx = self.x - checkX
-            local dy = self.y - checkY
+            local dx = math.abs(self.x - checkX)
+            local dy = math.abs(self.y - checkY)
             local distance = math.sqrt((dx * dx) + (dy * dy))
 
             if (distance <= self.r) then
-                return true
+                -- Стандартный удар ближе к углу
+
+                result.success = true
+                result.type = "corner"
+
+                if (dx >= dy) then
+                    result.rectSide = sideX
+                else
+                    result.rectSide = sideY
+                end
+
+            else
+                -- Удары о центр стороны
+
+                if ((self.y <= obj.y + obj.h) and (self.y >= obj.y)) then
+                    if ((sideX == "right") and (self.x-self.r <= checkX)) or ((sideX == "left") and (self.x-self.r >= checkX)) then
+                        result.success = true
+                        result.type = "side"
+                        result.rectSide = sideX
+                    end
+                elseif ((self.x <= obj.x + obj.w) and (self.x >= obj.x)) then
+                    if ((sideY == "bottom") and (self.y-self.r <= checkY)) or ((sideY == "top") and (self.y-self.r >= checkY)) then
+                        result.success = true
+                        result.type = "side"
+                        result.rectSide = sideY
+                    end
+                end
+
             end
         end
     end
 
-    return false
+    return result
 end
