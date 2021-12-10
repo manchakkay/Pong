fdbf3ef5a914533d9fa3ece9e5f83f3298a7c4c9
Manchakkay Maxim <mxmaster1203@live.com>
2021-12-10 18:47:40 +0800

    Updated game settings and added metainfo
    Game settings changed to more fast experience. Also added some metainformation

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ require "src.ui"
 
 function love.load()
     -- Полноэкранный режим и цвет фона
-    -- love.window.setFullscreen(true)
+    love.window.setFullscreen(true)
     love.graphics.setBackgroundColor(0 / 255, 0 / 255, 0 / 255)
 
     -- Получение размеров окна
@@ -14,15 +14,20 @@ function love.load()
     -- Установка настроек игры
     love.math.setRandomSeed(love.timer.getTime())
     gameConfig = {
+        metainfo = {
+            version_code = "0.1 RC",
+            author = "Manchakkay Maxim"
+        },
+
         paddleSpeed = 15,
         paddleWidth = 10,
         paddleHeight = 150,
 
-        ballRadius = 10,
-        ballStartSpeed = 7,
-        ballAcceleration = 0.002,
+        ballRadius = 14,
+        ballStartSpeed = 10,
+        ballAcceleration = 0.0025,
 
-        anglePower = 0.05
+        anglePower = 0.075
     }
 
     -- Создание инстанции игры
