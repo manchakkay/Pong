ab26600379f8ba8e41e21b0b574ecb2671babbe3
Manchakkay Maxim <mxmaster1203@live.com>
2021-12-10 18:46:30 +0800

    Decreased smoothing

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf( t )
     t.window.title = "Pong Game"
-    t.window.msaa = 16
+    t.window.msaa = 8
     t.window.vsync = 1
     t.window.width = 1920
     t.window.height = 1080
